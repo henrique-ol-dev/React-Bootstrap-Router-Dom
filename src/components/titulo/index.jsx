eaa842a4842cad1ba4e5997236adcc58c3525088
FIC <FIC@info.local>
2020-12-11 00:58:10 -0300

    Crud De Categorias e Upload de Imagens- Aula12

---
+++ src/components/titulo/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Jumbotron} from 'react-bootstrap';
+
+const Titulo = ({titulo, chamada}) => {
+    return (
+        <Jumbotron>
+            <h1>{titulo}</h1>
+            <p>
+                {chamada}
+            </p>
+        </Jumbotron>
+    )
+}
+
+export default Titulo;
